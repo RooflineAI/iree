4a217e94f2d0206bb2586184b3214271721df294
Florian Walbroel <florian.walbroel@rwth-aachen.de>
2025-06-27 13:43:57 +0000

    python bindings: add version to __init__.py files

--- compiler/bindings/python/iree/compiler/__init__.py
+++ compiler/bindings/python/iree/compiler/__init__.py
@@ -9,3 +9,21 @@
 # Re-export some legacy APIs from the tools package to this top-level.
 # TODO: Deprecate and remove these names once clients are migrated.
 from .tools import *
+
+# Roofline AI ->
+import sys
+
+if sys.version_info[:2] >= (3, 8):
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Note that the package name must be kept in sync with
+    # roof-iree/runtime/BUILD.bazel
+    __version__ = version("iree_compiler_bazel")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+# <- Roofline AI
